727a731574a82791d3a2e1a48362be3ae2a814c8
Naeem Ahmed <37439816+Naeem966@users.noreply.github.com>
2026-04-02 12:09:18 +0800

    Add technical skills data for Naeem

---
+++ src/data/skillsData.ts
@@ -0,0 +1,38 @@
+export const skillsData = {
+  programmingLanguages: {
+    JavaScript: 'Expert',
+    TypeScript: 'Intermediate',
+    Python: 'Proficient',
+    Java: 'Intermediate',
+    C++: 'Proficient'
+  },
+  frontendTechnologies: {
+    React: 'Proficient',
+    HTML_CSS: 'Expert',
+    TailwindCSS: 'Proficient',
+    Three_js: 'Intermediate',
+    GSAP: 'Intermediate'
+  },
+  backendTechnologies: {
+    Node_js: 'Proficient',
+    Express: 'Proficient',
+    MongoDB: 'Proficient',
+    PostgreSQL: 'Intermediate'
+  },
+  iotAndHardwareSkills: {
+    Arduino: 'Proficient',
+    sensors: 'Intermediate',
+    embeddedSystems: 'Intermediate'
+  },
+  developmentTools: {
+    Git: 'Expert',
+    Docker: 'Proficient',
+    VS_Code: 'Proficient',
+    Postman: 'Intermediate'
+  },
+  specializations: {
+    IoT: 'Expert',
+    smartSystems: 'Proficient',
+    fullStackDevelopment: 'Proficient'
+  }
+};
